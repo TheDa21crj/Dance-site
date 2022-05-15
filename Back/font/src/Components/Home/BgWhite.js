7cb80aebf4f54d9f3800dce4b665adfe1a646404
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-05-15 11:35:37 +0530

    v2.0

--- Back/font/src/Components/Home/BgWhite.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./CSS/Home.css";
-
-export default function BgWhite() {
-  return <div id="bg_white"></div>;
-}
--- Back/font/src/Pages/Error.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ErrorC from "../Components/Error/Error";
-export default function Error() {
-  return (
-    <div>
-      <ErrorC />
-    </div>
-  );
-}
